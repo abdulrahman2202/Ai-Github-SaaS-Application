49efba83448804a51aab8d82ecb7f8e5c7d2d949
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2025-12-28 19:54:52 +0530

    project section shown done

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Sidebar, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { cn } from "@/lib/utils"
 import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react"
 import Link from "next/link"
@@ -28,6 +28,17 @@ const items = [
         icon: CreditCard
     },
 ]
+const projects = [
+    {
+        name:"Project 1"
+    },
+    {
+        name:"Project 2"
+    },
+    {
+        name:"Project 3"
+    },
+]
 
 export function AppSidebar(){
     const pathname = usePathname()
@@ -36,6 +47,8 @@ export function AppSidebar(){
             <SidebarHeader>
                 Logo
             </SidebarHeader>
+
+            <SidebarContent>
             <SidebarGroup>
                 <SidebarGroupLabel>
                     Application
@@ -59,6 +72,38 @@ export function AppSidebar(){
                     </SidebarMenu>
                 </SidebarGroupContent>
             </SidebarGroup>
+
+            <SidebarGroup>
+                <SidebarGroupLabel>
+                    Your Projects
+                </SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        {projects.map(project =>{
+                            return(
+                                <SidebarMenuItem key={project.name}>
+                                    <SidebarMenuButton asChild>
+                                        <div>
+                                            <div className={cn(
+                                                'rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary',
+                                                {
+                                                'bg-primary text-white' : true
+                                                }
+                                            )}>
+                                                {project.name[0]}
+                                            </div>
+                                            <span>{project.name}</span>
+                                        </div>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            )
+                        })}
+                 
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+            
+            </SidebarContent>
         </Sidebar>
     )
 
