6d84d39f6a6af7a44c5cc41294a54cc092ef2e29
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2025-12-29 20:07:06 +0530

    project is stored in the database with the help of project route backend

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -2,7 +2,9 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { api } from "@/trpc/react"
 import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 
 type FormInput = {
     repoUrl: string
@@ -12,9 +14,22 @@ type FormInput = {
 
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
+    const createProject = api.project.createProject.useMutation()
 
     function onSubmit(data: FormInput) {
-        window.alert(JSON.stringify(data,null,2))
+        createProject.mutate({
+            githubUrl:data.repoUrl,
+            name:data.projectName,
+            githubToken:data.githubToken
+        },{
+            onSuccess: () =>{
+                toast.success('Project created successfully')
+                reset()
+            },
+            onError: () => {
+                toast.error("Failed to create project")
+            }
+        })
         return true
     }
     return (
@@ -56,7 +71,7 @@ const CreatePage = () => {
                         />
 
                         <div className="h-4"></div>
-                        <Button type="submit">
+                        <Button type="submit" disabled={createProject.isPending}>
                             Create Project
                         </Button>
 
