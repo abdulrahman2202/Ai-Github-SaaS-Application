b8f37107188f194e9f9143519081372122443413
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2025-12-30 21:44:25 +0530

    working on commit section

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,5 +1,73 @@
+import { db } from '@/server/db';
 import { Octokit } from 'octokit';
 
 export const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
 })
+
+const githubUrl = 'https://github.com/docker/genai-stock'
+
+type Response = {
+    commitHash:string
+    commitMessage:string
+    commitAuthorName:string
+    commitAuthorAvatar:string
+    commitDate:string
+}
+
+export const getCommitHashes = async(githubUrl:string): Promise<Response[]> =>{
+    const [owner,repo] = githubUrl.split('/').slice(-2)
+    if(!owner || !repo){
+        throw new Error("Invalid github url")
+    }
+    const {data} = await octokit.rest.repos.listCommits({
+        owner,
+        repo
+    })
+    const sortedCommits = data.sort((a: any,b:any)=> new Date(b.commit.author.date).getTime() - new Date(a.commit.author.date).getTime()) as any[]
+
+    return sortedCommits.slice(0,10).map((commit:any)=>({
+        commitHash: commit.sha as string,
+        commitMessage: commit.commit.message ?? "",
+        commitAuthorName: commit.commit?.author?.name ?? "",
+        commitAuthorAvatar: commit?.author?.avatar_url ?? "",
+        commitDate:commit.commit?.author?.data ?? ""
+        
+    }))
+}
+
+export const pollCommits = async (projectId:string) =>{
+    const {project,githubUrl} = await fetchProjectGithubUrl(projectId)
+    const commitHashes = await getCommitHashes(githubUrl)
+    const unprocessedCommits = await filterUnprocessedCommits(projectId,commitHashes)
+    return unprocessedCommits
+}
+
+async function summariesCommit(githubUrl:string,commitHash:string){
+
+}
+
+async function fetchProjectGithubUrl(projectId:string) {
+    const project = await db.project.findUnique({
+        where:{id:projectId},
+        select:{
+            githubUrl:true
+        }
+
+    })
+    if(!project?.githubUrl){
+        throw new Error("Project has no github url")
+
+    }
+    return {project,githubUrl: project.githubUrl}
+}
+
+async function filterUnprocessedCommits(projectId:string, commitHashes: Response[]) {
+    const processedCommits = await db.commit.findMany({
+        where:{projectId}
+    })
+    const unprocessedCommits = commitHashes.filter((commit) => !processedCommits.some((processedCommits) => processedCommits.commitHash === commit.commitHash))
+    return unprocessedCommits
+}
+
+await pollCommits("cmjr9e4bc0000s4g0nivoz5y6").then(console.log)
